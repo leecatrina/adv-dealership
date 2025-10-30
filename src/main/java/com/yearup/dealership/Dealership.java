91c042caf6aaa803d341f5c654a36eddee735738
leecatrina <cninalee3@gmail.com>
2025-10-30 13:35:05 -0400

    add vehicle, remove vehicle

---
+++ src/main/java/com/yearup/dealership/Dealership.java
@@ -0,0 +1,33 @@
+package com.yearup.dealership;
+
+import java.util.ArrayList;
+
+public class Dealership {
+    private String name;
+    private String address;
+    private String phone;
+
+    private ArrayList<Vehicle> inventory;
+
+    public Dealership(String name, String address, String phone, ArrayList<Vehicle> inventory) {
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+        this.inventory = inventory;
+        this.inventory = new ArrayList<>();
+
+    }
+
+    public ArrayList<Vehicle> getAllVehicles() {
+        return inventory;
+    }
+
+    public void addVehicle(Vehicle vehicle) {
+        this.inventory.add(vehicle);
+    }
+
+    public void removeVehicle(Vehicle vehicle) {
+        this.inventory.remove(vehicle);
+    }
+}
+
---
+++ src/main/java/com/yearup/dealership/UserInterface.java
@@ -0,0 +1,14 @@
+package com.yearup.dealership;
+
+public class UserInterface {
+
+
+    public UserInterface(){
+
+    }
+
+    public void display(){
+
+        System.out.println("Dealership App");
+    }
+}
